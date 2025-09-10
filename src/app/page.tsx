1b584841fd6746422e2059ebe74c3dfd50c7543e
Aditi Naik <aditinaik277@gmail.com>
2025-09-10 22:50:38 +0530

    [Edited] Metadata added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,9 @@ export const metadata = {
   keywords: "Aditi Naik, Aditi S Naik, Full Stack Developer, Portfolio, Web Developer",
   robots: "index, follow",  // allows indexing
   authors: [{ name: "Aditi S Naik" }],
+    other: [
+    { name: "google-site-verification", content: "yzJkq9HILjHwdtnpc4J_c2MNJZ1753oI1DWXQZe0mLe8" }
+  ]
 };
 
 export default function Home() {
