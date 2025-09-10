4d0ac4495c5de358b9cb6007f5dc0d7103de3cb3
Aditi Naik <aditinaik277@gmail.com>
2025-09-10 22:18:36 +0530

    [Edited] Metadata added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,23 @@
-
 import MainPage from "@/app/mainPage/page"
+
+export const metadata = {
+  title: 'Aditi S Naik | Full Stack Developer',
+  description: 'Portfolio of Aditi S Naik, Full Stack Developer',
+  icons: '/tech_girl.jpg',
+};
 
 export default function Home() {
   return (
    <>
 
         {/* Add your fonts or meta here */}
-        <link
+   
+
+       
+    <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
           rel="stylesheet"
         />
-  
    <MainPage/>
     </>
   
